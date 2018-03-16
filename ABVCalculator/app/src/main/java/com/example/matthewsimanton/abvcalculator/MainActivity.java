da5f91d00370ce6574596214c2f7a9b9166221c1
msimanton <matthew.simanton@seattlecentral.edu>
2018-03-15 20:45:02 -0700

    Changed theme

--- ABVCalculator/app/src/main/java/com/example/matthewsimanton/abvcalculator/MainActivity.java
+++ ABVCalculator/app/src/main/java/com/example/matthewsimanton/abvcalculator/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.matthewsimanton.abvcalculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
+
 
 public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
 
@@ -28,6 +36,8 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -46,6 +56,14 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
 
         savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
 
+
+
+    }
+
+    public void displaySettings(View view) {
+        //start settings activity
+        startActivity(new Intent(this,SettingsActivity.class));
+        //Intent intent = new Intent(this,SettingsActivity.class);
     }
 
     @Override
@@ -87,6 +105,33 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
 
         textViewABV.setText(Float.toString(calcABV));
     }
+
+    //displays options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    //handle the menu events
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+
+            case R.id.help:
+                startActivity(new Intent(this, HelpActivity.class));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
 
     @Override
     public void onPause() {
