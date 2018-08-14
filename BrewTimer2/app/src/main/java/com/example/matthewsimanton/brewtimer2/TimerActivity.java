2c81de7beb65a31d53abde2c4748b19b74376313
msimanton <matthew.simanton@seattlecentral.edu>
2018-08-14 03:18:20 -0700

    I still don't have the alarm set up correctly

---
+++ BrewTimer2/app/src/main/java/com/example/matthewsimanton/brewtimer2/TimerActivity.java
@@ -0,0 +1,232 @@
+package com.example.matthewsimanton.brewtimer2;
+
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.concurrent.TimeUnit;
+
+public class TimerActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private long timeCountInMilliSeconds = 60 * 60000;
+
+    private enum TimerStatus {
+        STARTED,
+        STOPPED
+    }
+
+    private TimerStatus timerStatus = TimerStatus.STOPPED;
+
+    private ProgressBar progressBarCircle;
+//    private EditText editTextMinute;
+    private TextView textViewTime;
+    private ImageView imageViewReset;
+    private ImageView imageViewStartStop;
+    private CountDownTimer countDownTimer;
+    private ToggleButton button1;
+    private ToggleButton button2;
+    private ToggleButton button3;
+    private ToggleButton button4;
+    private ToggleButton button5;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+        button1 = (ToggleButton)findViewById(R.id.button1);
+        button2 = (ToggleButton)findViewById(R.id.button2);
+        button3 = (ToggleButton)findViewById(R.id.button3);
+        button4 = (ToggleButton)findViewById(R.id.button4);
+        button5 = (ToggleButton)findViewById(R.id.button5);
+
+
+
+
+
+
+        // method call to initialize the views
+        initViews();
+        // method call to initialize the listeners
+        initListeners();
+
+
+    }
+
+    // method to initialize the views
+    private void initViews() {
+        progressBarCircle = (ProgressBar) findViewById(R.id.progressBarCircle);
+        textViewTime = (TextView) findViewById(R.id.textViewTime);
+        imageViewReset = (ImageView) findViewById(R.id.imageViewReset);
+        imageViewStartStop = (ImageView) findViewById(R.id.imageViewStartStop);
+    }
+
+    //method to initialize the click listeners
+    private void initListeners() {
+        imageViewReset.setOnClickListener(this);
+        imageViewStartStop.setOnClickListener(this);
+    }
+
+    //implemented method to listen clicks
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageViewReset:
+                reset();
+                break;
+            case R.id.imageViewStartStop:
+                startStop();
+                break;
+        }
+    }
+
+    //method to reset timer
+    private void reset() {
+        stopCountDownTimer();
+        startCountDownTimer();
+    }
+
+
+    //method to start/stop timer
+    private void startStop() {
+        if (timerStatus == TimerStatus.STOPPED) {
+
+            // call to initialize the timer values
+            setTimerValues();
+            // call to initialize the progress bar values
+            setProgressBarValues();
+            // showing the reset icon
+            imageViewReset.setVisibility(View.VISIBLE);
+            // changing play icon to stop icon
+            imageViewStartStop.setImageResource(R.drawable.icon_stop);
+            // changing the timer status to started
+            timerStatus = TimerStatus.STARTED;
+            // call to start the count down timer
+            startCountDownTimer();
+
+        } else {
+
+            // hiding the reset icon
+            imageViewReset.setVisibility(View.GONE);
+            // changing stop icon to start icon
+            imageViewStartStop.setImageResource(R.drawable.icon_start);
+            // changing the timer status to stopped
+            timerStatus = TimerStatus.STOPPED;
+            stopCountDownTimer();
+
+        }
+
+    }
+
+    //method to initialize the values for timer
+    private void setTimerValues() {
+        int time = 60;
+
+        timeCountInMilliSeconds = time * 60 * 1000;
+
+    }
+
+    //method to start timer
+    private void startCountDownTimer() {
+
+        countDownTimer = new CountDownTimer(timeCountInMilliSeconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                textViewTime.setText(hmsTimeFormatter(millisUntilFinished));
+
+                progressBarCircle.setProgress((int) (millisUntilFinished / 1000));
+
+                int alarmTime = (int) (millisUntilFinished / 100);
+
+
+                //set toggle buttons to go sound alarm
+                if(button1.isChecked() && alarmTime==59) {
+                    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                    r.play();
+                } else if (button2.isChecked() && alarmTime==30) {
+                    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                    r.play();
+                } else if (button3.isChecked() && alarmTime==15) {
+                    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                    r.play();
+                } else if (button4.isChecked() && alarmTime==10) {
+                    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                    r.play();
+                } else if (button5.isChecked() && alarmTime==5) {
+                    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                    r.play();
+                }
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                textViewTime.setText(hmsTimeFormatter(timeCountInMilliSeconds));
+                // call to initialize the progress bar values
+                setProgressBarValues();
+                // hiding the reset icon
+                imageViewReset.setVisibility(View.GONE);
+                // changing stop icon to start icon
+                imageViewStartStop.setImageResource(R.drawable.icon_start);
+                // changing the timer status to stopped
+                timerStatus = TimerStatus.STOPPED;
+                //play notification when done
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                r.play();
+            }
+
+        }.start();
+        countDownTimer.start();
+    }
+
+    //method to stop timer
+    private void stopCountDownTimer() {
+        countDownTimer.cancel();
+    }
+
+    //method to set circular progress bar values
+    private void setProgressBarValues() {
+
+        progressBarCircle.setMax((int) timeCountInMilliSeconds / 1000);
+        progressBarCircle.setProgress((int) timeCountInMilliSeconds / 1000);
+    }
+
+
+    //method to convert millisecond to time format
+    private String hmsTimeFormatter(long milliSeconds) {
+
+        String hms = String.format("%02d:%02d:%02d",
+                TimeUnit.MILLISECONDS.toHours(milliSeconds),
+                TimeUnit.MILLISECONDS.toMinutes(milliSeconds) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliSeconds)),
+                TimeUnit.MILLISECONDS.toSeconds(milliSeconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliSeconds)));
+
+        return hms;
+
+
+    }
+
+
+}
